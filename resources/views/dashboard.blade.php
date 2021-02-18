1bcb9a734287003dd15a0f0855887586257f3d14
fahreza <74294398+mdsvmr@users.noreply.github.com>
2021-02-18 12:59:58 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,11 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
     
-                <a href="/">
-                    <img class="w-36" src="{{ asset('img/ewallet-logo.png')}} " alt="Logo">
+                <a>
+                    <img style="width: 300px" src="{{ asset('img/ewallet-logo.png')}} " alt="Logo">
                 </a>
                 
-                <h1>Grafik</h1>
+                <h1 style="font-size: 20px; padding: 0 20px 30px;">
+                    E-Wallet will manage your income and expenses by saving it with no charge and feel how easy to manage your personal finance and payment.
+                </h1>
                     
                 </table>
             </div>
